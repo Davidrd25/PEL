fed25a2bb32ea90ecc80f1512cfa809225c9291a
David <davichi1385@gmail.com>
2022-11-14 17:15:14 +0100

    Organizacion de las carpetas

--- Ejercicios/Ejercicio3/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Proyectos/Ejercicio3
-# Build directory: C:/Proyectos/Ejercicio3/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
